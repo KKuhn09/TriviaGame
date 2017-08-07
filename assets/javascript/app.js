d4490da95f0794bd734417694e4bde40d2c33461
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-08-06 23:06:55 -0400

    secondCommit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,25 +2,38 @@ var timeLeft = 30;
 
 $(document).ready(function() {
 
-	//Need to make a function that only allows 1 checkbox to be checked, per form
 
 	//Need to give each answer properties of 'correct' and 'incorrect'
 
 	var timerId = setInterval(countdown, 1000);
+	var score = 0;
+
+	function checkAnswers() {
+		if(document.getElementById("correct1").checked){
+			score++;
+		}
+		if(document.getElementById("correct2").checked){
+			score++;
+		}
+		if(document.getElementById("correct3").checked){
+			score++;
+		}
+	}
+
+	function endGame() {
+		window.alert("Your score is: " + score);
+	}
 
 	function countdown() {
 	  if (timeLeft == 0) {
 	    clearTimeout(timerId);
-	    //endGame function
+	    checkAnswers();
+	    endGame();
 	  } 
 	  else {
 	    $("#div1").html("Time Remaining: " + timeLeft + " seconds")
 	    timeLeft--;
 	  }
 	}
-
-	//endGame function
-	//Will report back to the user what they answered, and if it was correct or not
-	//Will give the user a score based on correct answers
 
 });
