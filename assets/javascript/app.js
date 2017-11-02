00b8f0b55d62adfe220fa9d97d51b98ffa279a29
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-11-02 14:07:47 -0600

    Game working, comments added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,53 +4,55 @@ var questions = [{
 	answers: ["Crab", "Mollusc", "Crayfish", "Lobster"],
 	correctAnswer: "Mollusc"
 },{
-	question: "How fast will gravity accelerate an object in free fall?",
-	answers: ["10 ft/s", "10 m/s", "9.8 ft/s squared", "9.8 m/s squared"],
-	correctAnswer: "9.8 m/s squared"
+	question: "Which of these appeared on Earth most recently?",
+	answers: ["Sharks", "Jellyfish", "Trees", "Nautiluses"],
+	correctAnswer: "Trees"
 },{
 	question: "About how many days does it take the moon to orbit the earth?",
 	answers: ["27", "10", "28", "30"],
 	correctAnswer: "27"
-}]
+}];
 
 var timer; 
-//Our game object
+//Contains everything needed for our game to run
 var game = {
 	//The games needed properties
 	correct: 0,
 	incorrect: 0,
-	counter: 10,
+	counter: 30,
 
 	//Count down the games timer
-	countdown: function(){
+	countDown: function(){
 		//Update count down every 1 second 
 		var x = setInterval(function(){
-			//Decrement counter
-			game.counter--;
-			//Display the result
-			$("#timer").html("Time Remaining: "+game.counter+" seconds");
-			//If the count down is finished
-			if(game.counter == 0){
-				clearInterval(x);
-				game.endGame();
+			game.counter--; //Decrement counter
+			$("#timer").html("Time Remaining: "+game.counter+" seconds"); //Display the result
+
+			if(game.counter == 0){ //If the count down is finished
+				clearInterval(x); //Stop the counter
+				game.endGame(); //End the game
 			}
+
 		}, 1000);
 	},
+
 	//Start the game
 	startGame: function(){
-		//Clear div3
-		$(".div3").empty();
-		//Display games timer
-		$(".div3").append("<h3 id='timer'>Time Remaining: "+game.counter+" seconds</h3><br>");
+		$(".div3").empty();  // Clear div3
+		this.countDown();  // Start the count down
+		//Display games initial timer
+		$(".div3").append("<h3 id='timer'>Time Remaining: "+this.counter+" seconds</h3><br>");
+
 		//For each item in our questions array, display the question
 		for(var i=0;i<questions.length;i++){
-			//Create a form area for each question
-			$(".div3").append("<form id='question"+i+"'></form><br>");
-			//Display the question
-			$("#question"+i).append("<h3>"+questions[i].question+"</h3>");
-			//For each answer choice, display it
+
+			$(".div3").append("<form id='question"+i+"'></form><br>");//Create a form area for each question
+			$("#question"+i).append("<h3>"+questions[i].question+"</h3>");//Display the question
+
+			//For each choice, display it
 			for(var j=0;j<questions[i].answers.length;j++){
-				$("#question"+i).append("<input type='radio' value='"+questions[i].correctAnswer+"'>"+questions[i].answers[j]);
+				$("#question"+i).append("<input type='radio' name='question-"+i+"' value='"+
+					questions[i].answers[j]+"'>"+questions[i].answers[j]);
 			}
 		}
 	},
@@ -58,29 +60,36 @@ var game = {
 	calculateScore: function(){
 		//For each question
 		for(var i=0;i<questions.length;i++){
-			if(console.log("Something");
+			//Select the checked radio button
+			$.each($("input[name='question-"+i+"']:checked"), function(){
+				//If value of checked button equals the questions answer
+				if ($(this).val() === questions[i].correctAnswer) {
+			        game.correct++; //Increment correct
+			      		}
+			    else {
+			       	game.incorrect++; //Increment incorrect
+			    }
+			});
 		}
 	},
 	//End the game
 	endGame: function(){
-		//Clear div3
-		$(".div3").empty();
-		//Calculate score
-		game.calculateScore();
+
+		game.calculateScore();//Calculate score
+		$(".div3").empty();//Clear div3
+
 		//Display players score
 		$(".div3").append("<h3>Score:</h3><br>");
-		$(".div3").append("<p>Correct Answers: "+game.correct+"</p>");
-		$(".div3").append("<p>Incorrect Answers: "+game.incorrect+"</p><br>");
+		$(".div3").append("<h3>Correct Answers: "+game.correct+"</h3>");
+		$(".div3").append("<h3>Incorrect Answers: "+game.incorrect+"</h3>");
+		$(".div3").append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
 	}
 }
 
 $(document).ready(function() {
 
-	//When the start button is clicked
-	$("#start-button").click(function(){
-		//Start the game
-		game.startGame();
-		game.countdown();
+	$("#start-button").click(function(){ // When the start button is clicked
+		game.startGame(); // Start the game
 	});
 
 });
