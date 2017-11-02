426edeab68a3881688a33b3fcb099d223f6e52dd
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-11-01 19:07:32 -0600

    Begun dissection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,35 @@
-var timeLeft = 30;
+var questions = [{
+	question: "Which of these is not a crustacean?",
+	answers: ["Crab", "Mollusc", "Crayfish", "Lobster"],
+	correctAnswer: "Mollusc"
+},{
+	question: "How fast will gravity accelerate an object in free fall?",
+	answers: ["10 ft/s", "10 m/s", "9.8 ft/s squared", "9.8 m/s squared"],
+	correctAnswer: "9.8 m/s squared"
+},{
+	question: "About how many days does it take the moon to orbit the earth?",
+	answers: ["27", "10", "28", "30"],
+	correctAnswer: "27"
+}]
+
+var timer; 
+
+var game = {
+
+	correct: 0,
+	incorrect: 0,
+	counter: 30,
+
+	countdown: function(){
+
+	},
+	
+	start: function(){
+
+	}
+
+}
 
 $(document).ready(function() {
-
-
-	//Need to give each answer properties of 'correct' and 'incorrect'
-
-	var timerId = setInterval(countdown, 1000);
-	var score = 0;
-
-	function checkAnswers() {
-		if(document.getElementById("correct1").checked){
-			score++;
-		}
-		if(document.getElementById("correct2").checked){
-			score++;
-		}
-		if(document.getElementById("correct3").checked){
-			score++;
-		}
-	}
-
-	function endGame() {
-		window.alert("Your score is: " + score);
-	}
-
-	function countdown() {
-	  if (timeLeft == 0) {
-	    clearTimeout(timerId);
-	    checkAnswers();
-	    endGame();
-	  } 
-	  else {
-	    $("#div1").html("Time Remaining: " + timeLeft + " seconds")
-	    timeLeft--;
-	  }
-	}
 
 });
