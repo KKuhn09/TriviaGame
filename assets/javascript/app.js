004c0a1825d4d89a76c2344b470cc2f01bc2db29
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-11-01 23:54:58 -0600

    Start button displays game when clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//Array of objects that stores our question data
 var questions = [{
 	question: "Which of these is not a crustacean?",
 	answers: ["Crab", "Mollusc", "Crayfish", "Lobster"],
@@ -13,23 +14,44 @@ var questions = [{
 }]
 
 var timer; 
-
+//Our game object
 var game = {
-
+	//The games needed variables
 	correct: 0,
 	incorrect: 0,
 	counter: 30,
 
+	//Function that counts down the game
 	countdown: function(){
-
+		//Update count down every 1 second 
+		var x = setInterval(function(){
+			
+		}, 1000);
 	},
-	
+	//Function that starts the game
 	start: function(){
-
+		//Clear div3
+		$(".div3").empty();
+		//Display games timer
+		$(".div3").append("<h3 id='timer'>Time Remaining:"+game.counter+" seconds</h3><br>");
+		//For each item in our questions array, display the question
+		for(var i=0;i<questions.length;i++){
+			$(".div3").append("<form id='question"+i+"'></form><br>");
+			$("#question"+i).append("<h3>"+questions[i].question+"</h3>");
+			for(var j=0;j<questions[i].answers.length;j++){
+				$("#question"+i).append("<input type='radio'>"+questions[i].answers[j]);
+			}
+		}
+		game.countdown();
 	}
-
 }
 
 $(document).ready(function() {
+
+	//When the start button is clicked
+	$("#start-button").click(function(){
+		//Start the game
+		game.start();
+	});
 
 });
