f81227b2d0684c06e45b22ef02ae40a0631a2e2e
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-11-02 01:03:21 -0600

    Timer works and ends game on count down

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,33 +16,61 @@ var questions = [{
 var timer; 
 //Our game object
 var game = {
-	//The games needed variables
+	//The games needed properties
 	correct: 0,
 	incorrect: 0,
-	counter: 30,
+	counter: 10,
 
-	//Function that counts down the game
+	//Count down the games timer
 	countdown: function(){
 		//Update count down every 1 second 
 		var x = setInterval(function(){
-			
+			//Decrement counter
+			game.counter--;
+			//Display the result
+			$("#timer").html("Time Remaining: "+game.counter+" seconds");
+			//If the count down is finished
+			if(game.counter == 0){
+				clearInterval(x);
+				game.endGame();
+			}
 		}, 1000);
 	},
-	//Function that starts the game
-	start: function(){
+	//Start the game
+	startGame: function(){
 		//Clear div3
 		$(".div3").empty();
 		//Display games timer
-		$(".div3").append("<h3 id='timer'>Time Remaining:"+game.counter+" seconds</h3><br>");
+		$(".div3").append("<h3 id='timer'>Time Remaining: "+game.counter+" seconds</h3><br>");
 		//For each item in our questions array, display the question
 		for(var i=0;i<questions.length;i++){
+			//Create a form area for each question
 			$(".div3").append("<form id='question"+i+"'></form><br>");
+			//Display the question
 			$("#question"+i).append("<h3>"+questions[i].question+"</h3>");
+			//For each answer choice, display it
 			for(var j=0;j<questions[i].answers.length;j++){
-				$("#question"+i).append("<input type='radio'>"+questions[i].answers[j]);
+				$("#question"+i).append("<input type='radio' value='"+questions[i].correctAnswer+"'>"+questions[i].answers[j]);
 			}
 		}
-		game.countdown();
+	},
+	//Calculate the players score
+	calculateScore: function(){
+		//For each question
+		for(var i=0;i<questions.length;i++){
+			if(console.log("Something");
+		}
+	},
+	//End the game
+	endGame: function(){
+		//Clear div3
+		$(".div3").empty();
+		//Calculate score
+		game.calculateScore();
+		//Display players score
+		$(".div3").append("<h3>Score:</h3><br>");
+		$(".div3").append("<p>Correct Answers: "+game.correct+"</p>");
+		$(".div3").append("<p>Incorrect Answers: "+game.incorrect+"</p><br>");
 	}
 }
 
@@ -51,7 +79,8 @@ $(document).ready(function() {
 	//When the start button is clicked
 	$("#start-button").click(function(){
 		//Start the game
-		game.start();
+		game.startGame();
+		game.countdown();
 	});
 
 });
